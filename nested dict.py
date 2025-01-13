0e7861e699ee396827d35b137f8a653c0eb0cf79
Rajankit27 <martialwarrior16@gmail.com>
2025-01-14 01:31:27 +0530

    Add files via upload

---
+++ nested dict.py
@@ -0,0 +1,10 @@
+student={
+    "name":"ankit raj",
+    "subjects":{
+        "physics":85,
+        "chemistry":90,
+        "maths":75
+    }
+}    
+#nested dictionary
+print(student["subjects"]["chemistry"])
